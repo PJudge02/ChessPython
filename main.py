2bdc4c76d87358decbe6e9250f09b87f99794f0d
Peter Judge <pjudge2002@gmail.com>
2022-09-15 15:39:08 -0400

    fix main

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from chess import Chess
-from gui import GUI
 
 def main():
-    gui = GUI()
-    gui.gameLoop()
 
-    # chess = Chess()
-    # print(chess.board.__str__())
+    chess = Chess()
+    print(chess.board.__str__())
 
-    # chess.game_loop()
+    chess.game_loop()
     print("Program Complete")
 
 if __name__=='__main__':
